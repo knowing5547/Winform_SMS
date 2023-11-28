b52049afc39bde4c377abaa2b39f81f073f7864d
sinbobo0712 <72530912+knowing5547@users.noreply.github.com>
2023-11-28 23:58:25 +0900

    Form2 / 주소록 수정 ver.5

--- winform_SMS/winform_SMS/Form2.Designer.cs
+++ winform_SMS/winform_SMS/Form2.Designer.cs
@@ -319,7 +319,6 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form2";
-            this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.groupBox1.ResumeLayout(false);
--- winform_SMS/winform_SMS/Form3.Designer.cs
+++ winform_SMS/winform_SMS/Form3.Designer.cs
@@ -213,7 +213,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form3";
-            this.Text = "Form3";
+            this.Text = "anzkz";
             this.Load += new System.EventHandler(this.Form3_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
